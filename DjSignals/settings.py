c1910486a026d6c80c02d6c924f24c6f6a2a4d35
Arifcse21 <arifcse21@gmail.com>
2023-03-18 10:13:36 +0600

    readme file added

--- DjSignals/settings.py
+++ DjSignals/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e*x_+p(9^&i!y$mq=ik%hi0@+7xks4gbf26*l+)#4k_k3n0)#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contents',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DjSignals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f"{BASE_DIR}/'templates'", f"{BASE_DIR}/ 'contents' / 'templates'"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
